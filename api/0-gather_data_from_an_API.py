28d1f5d4d0d33a7c8914d5508790d06a60245dfa
ydroal <droal.yoko@gmail.com>
2023-04-06 21:55:42 +0200

    update: task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,23 +12,20 @@ if __name__ == '__main__':
     if args[1]:
         employee_id = int(sys.argv[1])
 
-    res = requests.get(
-        'https://jsonplaceholder.typicode.com/users',
-        params={'id': employee_id}
-        )
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    res = requests.get(url)
     res_json = res.json()
-    employee_name = res_json[0].get('name')
+    employee_name = res_json.get('name')
 
-    res_todo = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-        )
+    url_todo = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    res_todo = requests.get(url_todo)
     res_todo_json = res_todo.json()
     total_tasks = len(res_todo_json)
     done_tasks = [task for task in res_todo_json if task['completed']]
 
     print(f'Employee {employee_name} is done with tasks'
           f'({len(done_tasks)}/{total_tasks}):')
-
+    
     for task in done_tasks:
         task_title = task.get('title')
         print(f'\t {task_title}')
