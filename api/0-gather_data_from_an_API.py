cd0d6c7cbce882a739c2a3dfc85086daba5f2b1f
ydroal <droal.yoko@gmail.com>
2023-04-07 10:22:39 +0200

    add: convert file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,24 +11,27 @@ if __name__ == '__main__':
     args = sys.argv
     if args[1]:
         employee_id = int(sys.argv[1])
+    else:
+        employee_id = None
+        exit()
 
-    res = requests.get(
-        'https://jsonplaceholder.typicode.com/users',
-        params={'id': employee_id}
-        )
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    res = requests.get(url)
     res_json = res.json()
-    employee_name = res_json[0].get('name')
+    employee_name = res_json.get('name')
 
-    res_todo = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-        )
+    url_todo = ('https://jsonplaceholder.typicode.com/'
+                f'users/{employee_id}/todos')
+
+
+    res_todo = requests.get(url_todo)
     res_todo_json = res_todo.json()
     total_tasks = len(res_todo_json)
     done_tasks = [task for task in res_todo_json if task['completed']]
 
     print(f'Employee {employee_name} is done with tasks'
           f'({len(done_tasks)}/{total_tasks}):')
-
+    
     for task in done_tasks:
         task_title = task.get('title')
         print(f'\t {task_title}')
